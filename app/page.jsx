2312dc00166dd703c4989bcd61f630c72aded99e
olazzi <tarihenes@gmail.com>
2025-04-16 21:56:18 -0400

    test

--- app/page.jsx
+++ app/page.jsx
@@ -2,66 +2,46 @@ import Link from 'next/link';
 import { Card } from 'components/card';
 import { ContextAlert } from 'components/context-alert';
 import { Markdown } from 'components/markdown';
-import { RandomQuote } from 'components/random-quote';
-import { getNetlifyContext } from 'utils';
 
 const contextExplainer = `
-The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
-([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
+If you wish to permanently delete your account, please follow the instructions below. 
+This will erase all your data and remove your access to the platform.
 `;
 
-const preDynamicContentExplainer = `
-The card content below is fetched by the client-side from \`/quotes/random\` (see file \`app/quotes/random/route.js\`) with a different quote shown on each page load:
+const postDeletionExplainer = `
+Please note that once your account is deleted, this action cannot be undone. 
+We value your feedback, and if you decide to leave, please consider sharing your reasons with us. 
+If you change your mind, feel free to come back anytime.
 `;
 
-const postDynamicContentExplainer = `
-On Netlify, Next.js Route Handlers are automatically deployed as [Serverless Functions](https://docs.netlify.com/functions/overview/).
-Alternatively, you can add Serverless Functions to any site regardless of framework, with acccess to the [full context data](https://docs.netlify.com/functions/api/).
-
-And as always with dynamic content, beware of layout shifts & flicker! (here, we aren't...)
-`;
-
-const ctx = getNetlifyContext();
-
-export default function Page() {
+export default function AccountDeletionPage() {
     return (
         <div className="flex flex-col gap-12 sm:gap-16">
             <section>
                 <ContextAlert className="mb-6" />
-                <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
-                <p className="mb-6 text-lg">Get started with Next.js and Netlify in seconds.</p>
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
-                    Read the Docs
+                <h1 className="mb-4">Account Deletion</h1>
+                <p className="mb-6 text-lg">
+                    We’re sorry to see you go. If you’re sure about deleting your account, please follow the steps below.
+                </p>
+                <Link href="https://play.google.com/store/apps/details?id=com.havoca" className="btn btn-lg sm:min-w-64">
+                    Go to Play Store
                 </Link>
             </section>
-            {!!ctx && (
-                <section className="flex flex-col gap-4">
-                    <Markdown content={contextExplainer} />
-                    <RuntimeContextCard />
-                </section>
-            )}
+
             <section className="flex flex-col gap-4">
-                <Markdown content={preDynamicContentExplainer} />
-                <RandomQuote />
-                <Markdown content={postDynamicContentExplainer} />
+                <Markdown content={contextExplainer} />
+                <Card title="Important Information">
+                    <p>If you delete your account, all your data will be permanently removed from our servers.</p>
+                    <p>If you would like to proceed, click the button below.</p>
+                </Card>
+                <button className="btn btn-lg bg-red-600 text-white">
+                    Delete My Account
+                </button>
+            </section>
+
+            <section className="flex flex-col gap-4">
+                <Markdown content={postDeletionExplainer} />
             </section>
         </div>
     );
-}
-
-function RuntimeContextCard() {
-    const title = `Netlify Context: running in ${ctx} mode.`;
-    if (ctx === 'dev') {
-        return (
-            <Card title={title}>
-                <p>Next.js will rebuild any page you navigate to, including static pages.</p>
-            </Card>
-        );
-    } else {
-        return (
-            <Card title={title}>
-                <p>This page was statically-generated at build time.</p>
-            </Card>
-        );
-    }
 }
